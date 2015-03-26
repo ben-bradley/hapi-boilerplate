4d7202e7088faf8d331c954d2871c394a5587839
Ben Bradley <[ben.bradley@cigna.com|mailto:ben.bradley@cigna.com]>
2015-03-26 15:11:22 -0700

    moved tests into plugins

--- test/spec.js
+++ test/spec.js
@@ -1,40 +1,7 @@
-var should = require('should'),
-  request = require('supertest'),
-  config = require('config');
+var glob = require('glob');
 
 var server = require('..');
 
-var API = request('http://localhost:' + config.api_port), // for calls to the API
-  UI = request('http://localhost:' + config.ui_port); // for calls to the UI
-
-describe('Your App', function () {
-
-  describe('The API', function () {
-
-    // start the server for each test
-    beforeEach(function(done) {
-      server.start(done);
-    });
-
-    // drop the server after each test
-    afterEach(function(done) {
-      server.stop(done);
-    });
-
-    // This is what a test should/can look like
-    it('GET /loopback should return data', function (done) {
-      API.get('/loopback')
-        .expect(200)
-        .expect(function (res) {
-          var json = res.body;
-          (json).should.be.an.Object;
-          (json).should.have.properties(['headers', 'info', 'method', 'params', 'path', 'payload', 'query', 'ts']);
-          (json.path).should.equal('/loopback');
-          (json.method).should.equal('get');
-        })
-        .end(done);
-    });
-
-  }); // end REST API
-
+glob.sync(__dirname+'/../plugins/*/test/*.js').forEach(function(file) {
+  require(file)(server);
 });
